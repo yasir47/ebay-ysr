948cbf5aeac619af25c7b9917ee66d04e1b9d5ae
Syed Muhammad Yasir <syed.yasir@tenpearls.com>
2015-07-30 12:38:14 +0500

    First commit

---
+++ lib/ebay/ysr.rb
@@ -0,0 +1,13 @@
+require "ebay/ysr/version"
+
+module Ebay
+  module Ysr
+    # Your code goes here...
+  end
+
+  def self.search
+
+  end
+
+
+end
---
+++ lib/ebay/ysr/version.rb
@@ -0,0 +1,5 @@
+module Ebay
+  module Ysr
+    VERSION = "0.1"
+  end
+end
